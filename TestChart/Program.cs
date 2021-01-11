f280340b1dcacab994d1dbcab362ef338c5cc833
Ben Torkington <prmera@gmail.com>
2021-01-11 13:54:19 +1300

    add charts

---
+++ TestChart/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using ScottPlot;
+
+namespace TestChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customCulture = new System.Globalization.CultureInfo("en-NZ");
+            customCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            int width = 800;
+            int height = 600;
+            var plot = new Plot(width, height);
+
+            DateTime[] myDates = new DateTime[60];
+            var startdate = DateTime.Now;
+            for (int i = 0; i < myDates.Length; i++)
+            {
+                myDates[i] = startdate.AddMinutes(i);
+            }
+
+            double[] dataX = myDates.Select(x => x.ToOADate()).ToArray();
+            //plot.PlotScatter(dataX, dataY);
+
+
+            plot.XAxis.DateTimeFormat(true);
+
+            plot.YAxis.Label("°C");
+            plot.YAxis.Color(Color.Red);
+
+            //double[] dataX = DataGen.Consecutive(60);
+            double[] dataY = DataGen.Sin(60, 2, 25, 5);
+            //plot.PlotScatter(dataX, dataY);
+
+            var scatter = plot.AddScatter(dataX, dataY);
+
+            scatter.LineWidth = 2;
+            scatter.Color = Color.Red;
+
+
+            plot.SaveFig("out.png");
+            var bitmap = plot.Render();
+            var png = ImageToPngByte(bitmap);
+
+
+            Console.WriteLine($"PNG byte size is {png.Length}");
+        }
+
+        public static byte[] ImageToPngByte(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
--- TwitterTest/Program.cs
+++ TwitterTest/Program.cs
@@ -6,6 +6,12 @@ using System.Linq;
 using TootingMad.DataSources.LaCrosse;
 using System.Collections.Generic;
 
+using ScottPlot;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Tweetinvi.Parameters;
+
 namespace TwitterTest
 {
     class TwitterCredentials
@@ -31,20 +37,11 @@ namespace TwitterTest
 
         static async Task Main(string[] args)
         {
-            TwitterCredentials creds;
-            try
-            {
-                creds = GetCredentials();
-            }
-            catch (ArgumentNullException ex)
-            {
-                Console.WriteLine("You need to set the WEATHERBOT_* environment variables with your Twitter API keys and secrets");
-                return;
-            }
+            TwitterCredentials creds = GetCredentials();
 
             var userClient = new TwitterClient(creds.ConsumerKey, creds.ConsumerSecret, creds.AccessToken, creds.AccessSecret);
 
-            var station = new TootingMad.DataSources.LaCrosse.WS2300(Environment.GetEnvironmentVariable("WEATHERBOT_SERIAL_PORT"));
+            //var station = new TootingMad.DataSources.LaCrosse.WS2300(Environment.GetEnvironmentVariable("WEATHERBOT_SERIAL_PORT"));
 
             var sensors = new List<Sensor>
             {
@@ -58,17 +55,56 @@ namespace TwitterTest
 
 
 
-            var res = station.GetSensors(sensors);
+            //var res = station.GetSensors(sensors);
 
-            var resultString = $"{((decimal)res[2].Item2).ToString("F1")}°C {((decimal)res[3].Item2).ToString("F0")}%RH {((decimal)res[1].Item2).ToString("F1")} kts {res[0].Item2} {((decimal)res[4].Item2).ToString("F1")} hPa(AMSL) {res[5].Item2}";
+            var resultString = $"Does this work?";
 
             Console.WriteLine(resultString);
 
             var user = await userClient.Users.GetAuthenticatedUserAsync();
             Console.WriteLine(user);
 
+
+            int width = 800;
+            int height = 600;
+            var plot = new Plot(width, height);
+
+            DateTime[] myDates = new DateTime[60];
+            var startdate = DateTime.Now;
+            for (int i = 0; i<myDates.Length; i++)
+            {
+                myDates[i] = startdate.AddMinutes(i * 5);
+            }
+
+            double[] dataX = myDates.Select(x => x.ToOADate()).ToArray();
+            double[] dataY = new double[] { 1, 4, 9, 16, 25, };
+            //plot.PlotScatter(dataX, dataY);
+
+            var scatter = plot.AddScatter(dataX, dataY);
+
+            plot.XAxis.DateTimeFormat(true);
+
+            //plot.SaveFig("out.png");
+            var bitmap = plot.Render();
+            var png = ImageToPngByte(bitmap);
+
+            var uploadImage = await userClient.Upload.UploadTweetImageAsync(png);
+            var tweetWithImage = await userClient.Tweets.PublishTweetAsync(new PublishTweetParameters(resultString)
+            {
+                Medias = { uploadImage }
+            });
+
             var tweet = await userClient.Tweets.PublishTweetAsync(resultString);
-            Console.WriteLine("published the tweet: " + tweet);
+            Console.WriteLine("published the tweet: " + tweetWithImage);
+        }
+
+        public static byte[] ImageToPngByte(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
     }
 }
